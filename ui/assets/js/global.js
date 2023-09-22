023b1fc467446aa5ccc810dba2276046a267db5b
AlleyER16 <rehobothmicahdaniels@gmail.com>
2023-09-22 20:33:20 +0100

    Done with first stage UI

--- ui/assets/js/global.js
+++ ui/assets/js/global.js
@@ -6,24 +6,3 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("navbar--open");
 });
-
-///////////////////////////////////////////////////////////
-// Sticky Navigation functionality
-const heroSectionEl = document.querySelector(".hero");
-const obs = new IntersectionObserver(
-  function (entries) {
-    const [ent] = entries;
-
-    if (!ent.isIntersecting) {
-      headerEl.classList.add("sticky");
-    } else {
-      headerEl.classList.remove("sticky");
-    }
-  },
-  {
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
-);
-obs.observe(heroSectionEl);
