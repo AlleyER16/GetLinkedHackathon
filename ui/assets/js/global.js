fc38b467c22fd2720f0550b6a98762491a3ab2ec
AlleyER16 <rehobothmicahdaniels@gmail.com>
2023-09-22 22:53:27 +0100

    Many more ui updates and patches

--- ui/assets/js/global.js
+++ ui/assets/js/global.js
@@ -1,3 +1,8 @@
+// Utility functions
+function convertRemToPixels(rem) {
+  return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
+
 ///////////////////////////////////////////////////////////
 // Navigation functionality
 const btnNavEl = document.querySelector(".header__toggle-main");
@@ -5,4 +10,34 @@ const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("navbar--open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling functionality because of safari does not
+// support the css property scroll-behavior: smooth
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href.startsWith("#")) {
+      if (href === "#") {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      } else {
+        const scrollEl = document.querySelector(href);
+        window.scroll({
+          top: scrollEl.offsetTop - convertRemToPixels(8.2),
+          behavior: "smooth",
+        });
+      }
+    } else {
+      window.location = href;
+    }
+
+    if (link.classList.contains("header__navbar-link")) {
+      headerEl.classList.toggle("navbar--open");
+    }
+  });
 });
